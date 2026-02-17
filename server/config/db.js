686b46de86b62b8e35a60afaf10525e739204478
shrikantnagane <shrikant.nagane2106@gmail.com>
2026-02-17 16:40:25 +0530

    Fix: Enable SSL for MySQL connection

--- server/config/db.js
+++ server/config/db.js
@@ -10,7 +10,10 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME || 'kideduquest',
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: true // Required for TiDB Cloud
+    }
 });
 
 module.exports = pool.promise();
